27a8c818aa8833de93cd5cc5bba40d608a04eec0
Joe Meissler <joe.meissler@gmail.com>
2026-04-29 09:16:38 -0700

    nvim: flash, only search at beginning of word

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -66,7 +66,13 @@ return {
         "s",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump()
+          require("flash").jump({
+            search = {
+              mode = function(str)
+                return "\\<" .. str
+              end,
+            },
+          })
         end,
         desc = "Flash",
       },
